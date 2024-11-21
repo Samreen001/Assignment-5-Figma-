9e3d7f9f775f61220647718a0bf66c9eea0c2567
Samreen001 <honey.hamid003@gmail.com>
2024-11-21 14:09:53 +0500

    first commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-[1357px] h-[70px] p-5' style={{backgroundColor:'rgb(162, 152, 117)' }}>
+        <h1 className='w-[439] text-[30px] font-bold text-white font-serif' >MANZZARI</h1>
+    </div>
+  )
+}
+export default Header
